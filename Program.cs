57af5cae6b3da76c9f5b7ab4993633ab8f062030
jcoruepersonal <jcorue@hotmail.com.ar>
2021-08-04 14:45:21 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuBeyondChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wordstream = new List<string>{
+                                     "Chill","Hello", "Great","Bye","Joe Doe",
+                                        "Hello","Bye","Hello","Green","Blue","Yellow",
+                                        "Dog","Cat","Apple","Apple","Bird","Green","Blue",
+                                        "Yellow","Dog","Cat","Bird","Green","Blue","Yellow",
+                                        "Dog","Cat","Bird","Green","Blue","Yellow","Dog","Cat",
+                                        "Bird","Apple"
+
+                                    };
+            
+            List<string> matrix = new List<string>{
+                                        "Bla","Hello","Ant","Idea",
+                                        "Great","Bye","Green",
+                                        "Blue","Yellow",
+                                        "Dog","Cat",
+                                        "Bird","Apple"
+                                    };
+
+            WordFinder wf = new WordFinder(matrix);
+
+            IEnumerable<string> result= wf.Find(wordstream);
+            
+
+        }
+    }
+}
---
+++ WordFinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuBeyondChallenge
+{
+    public class WordFinder
+    {
+        private IEnumerable<string> mat;
+        public WordFinder(IEnumerable<string> matrix)
+        {
+            mat = matrix;
+            
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> wordstream)
+        {
+            List<string> foundWords = new List<string>();
+            
+            if (wordstream.Count() > 0)
+            {
+
+                foundWords = wordstream.Where(p => mat.Contains(p)).ToList();
+                
+                return foundWords.GroupBy(pal => pal).OrderByDescending(pal => pal.Count()).Select(res => res.Key).ToList().Take(10);
+            }
+
+            else
+            {
+                return wordstream;
+            }
+        }        
+    }
+}
